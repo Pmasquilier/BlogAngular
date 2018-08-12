c19f894525c3978f0a0b94055e9b8ae069a5e61e
Pmasquilier <pierremasquilier@gmail.com>
2018-08-12 15:20:38 +0200

    creation du projet

---
+++ src/app/ma-publication/ma-publication.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ma-publication',
+  templateUrl: './ma-publication.component.html',
+  styleUrls: ['./ma-publication.component.scss']
+})
+export class MaPublicationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
